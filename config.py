d93bf926dd82bb744057f789d5117bc271ceecf2
child1212 <13146741545@163.com>
2020-10-15 15:21:37 +0800

    循环

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 AREA = 'dongcheng'
+AREA_LIST = ['dongcheng','xicheng','haidian','chaoyang','fengtai','shijingshan','tongzhou','changping','daxing','shunyi','yizhuangkaifaqu','fangshan','mentougou','pinggu','huairou','miyun','yanqing']
--- xiaoqu.py
+++ xiaoqu.py
@@ -10,49 +10,51 @@ def get_total_count(url_p):
     info_list = soup.find_all(attrs={'class':"total fl"})
     info = info_list[0].get_text()
     return int(info.split(' ')[1])
-AREA = 'xicheng'
-url_p = 'https://bj.ke.com/xiaoqu/{area}/'.format(area=AREA)
-total = get_total_count(url_p)
-if total % 30 == 0:
-    pages = (total//30)
-else:
-    pages = (total//30)+1
-for i in range(1,pages+1):
-    print(i)
-# for i in range(1,2):
-    if i == 1:
-        url = 'https://bj.ke.com/xiaoqu/{area}/'.format(area=AREA)
+for AREA in AREA_LIST:
+    print(AREA)
+    url_p = 'https://bj.ke.com/xiaoqu/{area}/'.format(area=AREA)
+    total = get_total_count(url_p)
+    if total % 30 == 0:
+        pages = (total//30)
     else:
-        url = 'https://bj.ke.com/xiaoqu/{area}/pg{num}/'.format(area=AREA,num=i)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text,'lxml')
-    info_list = soup.find_all(attrs={'class':'clear xiaoquListItem CLICKDATA'})
+        pages = (total//30)+1
+    for i in range(1,pages+1):
+        print('\n',i,'/',pages)
+    # for i in range(1,2):
+        if i == 1:
+            url = 'https://bj.ke.com/xiaoqu/{area}/'.format(area=AREA)
+        else:
+            url = 'https://bj.ke.com/xiaoqu/{area}/pg{num}/'.format(area=AREA,num=i)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,'lxml')
+        info_list = soup.find_all(attrs={'class':'clear xiaoquListItem CLICKDATA'})
 
-    # addr = info_list[0].find_all(attrs={'class':'positionInfo'})[0].get_text().replace('\n','\t').replace('\t',',').replace('|',',').replace(' ','').replace(',,',',').replace(',/',',')[1:-1:]
-    # price = info_list[0].find_all(attrs={'class':'totalPrice'})[0].get_text()
-    # subway = info_list[0].find_all(attrs={'class':'tagList'})[0].get_text()
-    db = pymysql.connect("localhost","root","123456","Houses")
-    cur_insert = db.cursor()
-    for info in info_list:
-        try:
-            xiaoqu = info.find_all(attrs={'class':'title'})[0].get_text().replace('\n','')
-            posi = info.find_all(attrs={'class':'positionInfo'})[0].get_text().replace('\n','\t').replace('\t',',').replace('|',',').replace(' ','').replace(',,',',').replace(',/',',')[1:-1:]
-            inf = posi.split(',')
-            addr,addr_1,typ,year = inf[0],inf[1],inf[2],inf[3]
-            price = info.find_all(attrs={'class':'totalPrice'})[0].get_text().replace('\n','')
-            subway = info_list[0].find_all(attrs={'class':'tagList'})[0].get_text().replace('\n','')
-            sql_insert ="""insert into xiaoqu(xiaoqu,area,addr,type,year,price,subway) values (\"{xiaoqu}\",\"{addr}\",\"{addr_1}\",\"{typ}\",\"{year}\",\"{price}\",\"{subway}\")""".format(xiaoqu=xiaoqu,addr=addr,addr_1=addr_1,year=year,typ=typ,price=price,subway=subway)
-        except:
-            pass
-        try:
-            cur_insert.execute(sql_insert)
-            # 提交
-            db.commit()
-            print('开始数据库插入操作',end=';')
-        except Exception as e:
-            db.rollback()
-            print('数据库插入操作错误回滚')
-    db.close()
+        # addr = info_list[0].find_all(attrs={'class':'positionInfo'})[0].get_text().replace('\n','\t').replace('\t',',').replace('|',',').replace(' ','').replace(',,',',').replace(',/',',')[1:-1:]
+        # price = info_list[0].find_all(attrs={'class':'totalPrice'})[0].get_text()
+        # subway = info_list[0].find_all(attrs={'class':'tagList'})[0].get_text()
+        db = pymysql.connect("localhost","root","123456","Houses")
+        cur_insert = db.cursor()
+        for info in info_list:
+            try:
+                xiaoqu = info.find_all(attrs={'class':'title'})[0].get_text().replace('\n','')
+                posi = info.find_all(attrs={'class':'positionInfo'})[0].get_text().replace('\n','\t').replace('\t',',').replace('|',',').replace(' ','').replace(',,',',').replace(',/',',')[1:-1:]
+                inf = posi.split(',')
+                addr,addr_1,typ,year = inf[0],inf[1],inf[2],inf[3]
+                price = info.find_all(attrs={'class':'totalPrice'})[0].get_text().replace('\n','')
+                subway = info_list[0].find_all(attrs={'class':'tagList'})[0].get_text().replace('\n','')
+                sql_insert ="""insert into xiaoqu(xiaoqu,area,addr,type,year,price,subway) values (\"{xiaoqu}\",\"{addr}\",\"{addr_1}\",\"{typ}\",\"{year}\",\"{price}\",\"{subway}\")""".format(xiaoqu=xiaoqu,addr=addr,addr_1=addr_1,year=year,typ=typ,price=price,subway=subway)
+            except:
+                pass
+            try:
+                cur_insert.execute(sql_insert)
+                # 提交
+                db.commit()
+                print('开始数据库插入操作',end=';')
+            except Exception as e:
+                db.rollback()
+                print('数据库插入操作错误回滚')
+        db.close()
+print("finish")
 
 
 
